c927e38dfcd0681c0a972582c43bb9c69b6bba0b
EC2 Default User <ec2-user@ip-172-31-17-124.us-west-1.compute.internal>
2022-06-18 01:16:57 +0000

    format

--- resources/views/contact/input.blade.php
+++ resources/views/contact/input.blade.php
@@ -4,36 +4,36 @@
 　　</head>
 　　<body>
         <h1>お問合せページです</h1>
-        
+
         <form class="contact-form" action="//www-creators.com/rsc/receiver.php" method="post">
-  <p>お客様の情報をご記載下さい。</p>
-  <div class="item">
-    <label class="label" for="name" name="name">名前</label>
-    <input id="name" type="text">
-  </div>
-  <div class="item">
-    <label class="label" for="e-mail">メール</label>
-    <input id="e-mail" type="email" name="email">
-  </div>
-  <div class="item">
-    <label class="label" for="message">本文</label>
-    <textarea rows="4" id="message" placeholder="ご意見をお寄せ下さい。" name="comment"></textarea>
-  </div>
-  <div class="item">
-    <p class="label">購入理由</p>
-    <div class="radio-group">
-      <label><input type="radio" name="source">友達から聞いた</label><br>
-      <label><input type="radio" name="source">CMを見た</label><br>
-      <label><input type="radio" name="source">ネット広告を見た</label>
-    </div>
-  </div>
-  <div class="item no-label">
-    <label><input type="checkbox" name="magazine">メルマガを希望する</label>
-  </div>
-  <div class="item no-label">
-    <input type="submit">
-  </div>
-</form>
+            <p>お客様の情報をご記載下さい。</p>
+            <div class="item">
+                <label class="label" for="name" name="name">名前</label>
+                    <input id="name" type="text">
+            </div>
+            <div class="item">
+                <label class="label" for="e-mail">メール</label>
+                    <input id="e-mail" type="email" name="email">
+            </div>
+            <div class="item">
+                <label class="label" for="message">本文</label>
+                    <textarea rows="4" id="message" placeholder="ご意見をお寄せ下さい。" name="comment"></textarea>
+            </div>
+            <div class="item">
+                <p class="label">購入理由</p>
+                <div class="radio-group">
+                    <label><input type="radio" name="source">友達から聞いた</label><br>
+                    <label><input type="radio" name="source">CMを見た</label><br>
+                    <label><input type="radio" name="source">ネット広告を見た</label>
+                </div>
+            </div>
+            <div class="item no-label">
+                <label><input type="checkbox" name="magazine">メルマガを希望する</label>
+            </div>
+            <div class="item no-label">
+                <input type="submit">
+            </div>
+        </form>
 　　</body>
 </html>
 
